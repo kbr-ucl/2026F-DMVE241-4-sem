7b4f40cfd189dfaf08e35542ef84eaa1e3318d00
Kaj Bromose <kbr@ucl.dk>
2026-01-17 16:42:05 +0100

    Init

--- DemoCode/Aspire101/Aspire101After/Aspire101After.AppHost/AppHost.cs
+++
@@ -1,9 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var serviceB = builder.AddProject<Projects.ServiceB>("serviceb");
-
-var serviceA = builder.AddProject<Projects.ServiceA>("servicea")
-    .WithReference(serviceB);
-
-builder.Build().Run();
-    
